5ce778f3fab2d0bab9aa7431115786f6fd4bb7f7
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2020-11-22 23:24:54 +0100

    libs up

---
+++ src/main/java/pl/dk/netstorageplay/NetStoragePlayApp.java
@@ -0,0 +1,12 @@
+package pl.dk.netstorageplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetStoragePlayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetStoragePlayApp.class, args);
+    }
+}
